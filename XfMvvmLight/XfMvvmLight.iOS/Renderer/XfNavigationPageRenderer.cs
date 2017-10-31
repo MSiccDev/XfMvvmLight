258b23fa7c9c5b327a70581ad8c2eafb4b9c0eb9
MSiccDev <msiccdev@hotmail.com>
2017-10-31 05:31:05 +0100

    fixed a bug in iOS page renderer for back button interception

--- XfMvvmLight/XfMvvmLight.iOS/Renderer/XfNavigationPageRenderer.cs
+++ XfMvvmLight/XfMvvmLight.iOS/Renderer/XfNavigationPageRenderer.cs
@@ -24,51 +24,58 @@ namespace XfMvvmLight.iOS.Renderer
             //making sure to use this only with non-modal pages
             if (Element is XfNavContentPage page && this.NavigationController != null)
             {
-                //disabling back swipe complettely:
-                this.NavigationController.InteractivePopGestureRecognizer.Enabled = false;
-
-                var backarrowImg = UIImage.FromBundle("arrow-back.png").ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
-
-                var backButton = new UIButton(UIButtonType.Custom)
+                var thisPageIndex = page.Navigation.NavigationStack.IndexOf(page);
+                if (thisPageIndex >= 1)
                 {
-                    HorizontalAlignment = UIControlContentHorizontalAlignment.Left,
-                    TitleEdgeInsets = new UIEdgeInsets(11.5f, 0f, 10f, 0f),
-                    //we need to move the image a bit more left to get closer to the OS-look
-                    ImageEdgeInsets = new UIEdgeInsets(1f, -8f, 0f, 0f)
-                };
+                    //disabling back swipe complettely:
+                    this.NavigationController.InteractivePopGestureRecognizer.Enabled = false;
 
-                //this makes sure we use the same behavior as the OS
-                //if there is no parent, it must throw an exception because something is wrong
-                //with the navigation structure
-                var parentStackIndex = page.Navigation.NavigationStack.IndexOf(page) - 1;
-                var parent = page.Navigation.NavigationStack[parentStackIndex];
-                backButton.SetTitle(string.IsNullOrEmpty(parent.Title) ? "Back" : parent.Title, UIControlState.Normal);
+                    var backarrowImg = UIImage.FromBundle("arrow-back.png")
+                        .ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
 
-                backButton.SetTitleColor(this.View.TintColor, UIControlState.Normal);
-                backButton.SetImage(backarrowImg, UIControlState.Normal);
-                backButton.SizeToFit();
-
-                backButton.TouchDown += (sender, e) =>
-                {
-                    if (!page.BlockBackNavigation)
+                    var backButton = new UIButton(UIButtonType.Custom)
                     {
-                        this.NavigationController.PopViewController(animated);
-                    }
-                    page.SendBackButtonPressed();
-                };
+                        HorizontalAlignment = UIControlContentHorizontalAlignment.Left,
+                        TitleEdgeInsets = new UIEdgeInsets(11.5f, 0f, 10f, 0f),
+                        //we need to move the image a bit more left to get closer to the OS-look
+                        ImageEdgeInsets = new UIEdgeInsets(1f, -8f, 0f, 0f)
+                    };
 
-                backButton.Frame = new CGRect(0, 0, UIScreen.MainScreen.Bounds.Width / 4, NavigationController.NavigationBar.Frame.Height);
+                    //this makes sure we use the same behavior as the OS
+                    //if there is no parent, it must throw an exception because something is wrong
+                    //with the navigation structure
+                    var parent = page.Navigation.NavigationStack[thisPageIndex - 1];
+                    backButton.SetTitle(string.IsNullOrEmpty(parent.Title) ? "Back" : parent.Title,
+                        UIControlState.Normal);
 
-                var view = new UIView(new CGRect(0, 0, backButton.Frame.Width, backButton.Frame.Height));
-                view.AddSubview(backButton);
+                    backButton.SetTitleColor(this.View.TintColor, UIControlState.Normal);
+                    backButton.SetImage(backarrowImg, UIControlState.Normal);
+                    backButton.SizeToFit();
+
+                    backButton.TouchDown += (sender, e) =>
+                    {
+                        if (!page.BlockBackNavigation)
+                        {
+                            this.NavigationController.PopViewController(animated);
+                        }
+                        page.SendBackButtonPressed();
+                    };
+
+                    backButton.Frame = new CGRect(0, 0, UIScreen.MainScreen.Bounds.Width / 4,
+                        NavigationController.NavigationBar.Frame.Height);
+
+                    var view = new UIView(new CGRect(0, 0, backButton.Frame.Width, backButton.Frame.Height));
+                    view.AddSubview(backButton);
 
 
-                var backButtonItem = new UIBarButtonItem(string.Empty, UIBarButtonItemStyle.Plain, null)
-                {
-                    CustomView = backButton
-                };
+                    var backButtonItem = new UIBarButtonItem(string.Empty, UIBarButtonItemStyle.Plain, null)
+                    {
+                        CustomView = backButton
+                    };
 
-                NavigationController.TopViewController.NavigationItem.SetLeftBarButtonItem(backButtonItem, animated);
+                    NavigationController.TopViewController.NavigationItem
+                        .SetLeftBarButtonItem(backButtonItem, animated);
+                }
             }
         }
     }
